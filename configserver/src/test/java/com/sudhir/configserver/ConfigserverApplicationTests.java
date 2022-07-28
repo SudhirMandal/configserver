c5ff4b45abb9088cf5301b9ecec710ba1f96384c
Sudhir <r.sudhirmandal@gmail.com>
2022-07-28 14:03:36 +0530

    Initial commit

---
+++ configserver/src/test/java/com/sudhir/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudhir.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
